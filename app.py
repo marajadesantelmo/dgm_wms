57c3efc4bc5f6676b75b41cd7b149630e7dece95
marajadesantelmo <50368116+marajadesantelmo@users.noreply.github.com>
2024-11-22 11:25:44 -0300

    xxx

--- app.py
+++ app.py
@@ -166,6 +166,7 @@ elif page == "Add Client":
     with st.form("add_client_form"):
         client_name = st.text_input("Client Name")
         contact = st.text_input("Contact")
+        email = st.text_input("e-mail")
         
         submitted = st.form_submit_button("Add Client")
         
@@ -173,8 +174,8 @@ elif page == "Add Client":
             # Insert new client into Supabase
             client_response = supabase_client.from_("clients").insert({
                 "Name": client_name,
-                "Contact": contact,
-                "Added": pd.Timestamp.now().isoformat()
+                "Phone": contact,
+                "Email": email,
             }).execute()
             
             if client_response.data:
