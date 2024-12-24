2c5b30d512a529a47f3880b8361c08149f8802c8
marajadesantelmo <50368116+marajadesantelmo@users.noreply.github.com>
2024-12-24 14:41:12 -0300

    metric3

--- page_dashboard.py
+++ page_dashboard.py
@@ -34,14 +34,17 @@ def show_page_dashboard():
 
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.subheader("Current Stock") 
         total_feet = current_stock['Total Length'].sum()
-        st.markdown(f"""
-        <div class="custom-metric">
+        col1_1, col1_2 = col1.columns([2, 1])
+        with col1_1:
+            st.subheader("Current Stock")
+        with col1_2:
+            st.markdown(f"""
+            <div class="custom-metric">
             <strong>Total Feets</strong><br>
             {total_feet:.0f}
-        </div>
-        """, unsafe_allow_html=True)
+            </div>
+            """, unsafe_allow_html=True)
         st.dataframe(current_stock, hide_index=True)
 
     with col2:
